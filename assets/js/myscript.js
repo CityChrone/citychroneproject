e924b8ce699c9f033ec15a31a8585ac25f21acaf
ocadni <ocadni@gmail.com>
2018-10-26 15:09:21 +0200

    new colored citychrone

---
+++ assets/js/myscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+
+	  //***** My script
+  console.log("before my script")
+  let citychrone_color = '<span class="orangeC">C</span>ity<span class="greenC">C</span>hro<span class="blueN">n</span>e'
+  $(".site-title").prepend(citychrone_color)
+  $(".page__title").prepend(citychrone_color)
+
+});
